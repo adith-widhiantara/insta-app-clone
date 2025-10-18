4d82ba9ae7d7c8e5030d060323de7959ace29bd7
Adith Widhiantara <aditya.saktyawan@javan.co.id>
2025-10-18 14:53:50 +0700

    US01-BE-02: Memverifikasi respons gagal jika Email/No. Telp/Username sudah terdaftar (duplikasi).

--- app/Http/Requests/Authentication/RegisterRequest.php
+++ app/Http/Requests/Authentication/RegisterRequest.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Requests\Authentication;
 
+use App\Models\User;
 use App\Http\Requests\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterRequest extends Request
 {
-    //
+    public function rules(): array
+    {
+        return array_merge(parent::rules(), [
+            'email' => [$this->required, Rule::unique((new User())->getTable(), 'email')],
+            'phone' => [$this->required, Rule::unique((new User())->getTable(), 'phone')]
+        ]);
+    }
+
 }
--- tests/Feature/Controllers/AuthenticationControllerTest.php
+++ tests/Feature/Controllers/AuthenticationControllerTest.php
@@ -86,4 +86,48 @@ class AuthenticationControllerTest extends TestCase
             'name' => $response->json('data.user.name'),
         ]);
     }
+
+    public function test_failed_register_email_is_registered(): void
+    {
+        $email = 'email@name.com';
+
+        $this->createUser([
+            'email' => $email
+        ]);
+
+        $response = $this->postJson('api/auth/register', [
+            'name' => 'Test Name',
+            'email' => $email,
+            'phone' => 'phonephone',
+            'password' => 'password'
+        ]);
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonValidationErrors([
+                'email'
+            ]);
+    }
+
+    public function test_failed_register_phone_is_registered(): void
+    {
+        $phone = 'phonephone';
+
+        $this->createUser([
+            'phone' => $phone
+        ]);
+
+        $response = $this->postJson('api/auth/register', [
+            'name' => 'Test Name',
+            'email' => 'email@name.com',
+            'phone' => $phone,
+            'password' => 'password'
+        ]);
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonValidationErrors([
+                'phone'
+            ]);
+    }
 }
