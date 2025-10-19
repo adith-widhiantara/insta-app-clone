c629d601052cdd34ea4bf028c06f075e573de0a4
Adith Widhiantara <aditya.saktyawan@javan.co.id>
2025-10-19 12:43:15 +0700

    US12-BE-02: Memverifikasi gagal jika hubungan follow sudah ada (duplikasi).

--- app/Services/FollowService.php
+++ app/Services/FollowService.php
@@ -3,21 +3,42 @@
 namespace App\Services;
 
 use App\Http\Requests\Request;
+use App\Models\Follow;
 use App\Models\User;
+use Illuminate\Validation\ValidationException;
 use Laravolt\Crud\CrudModel;
 
 class FollowService extends Service
 {
+    /**
+     * @throws ValidationException
+     */
     public function followUser(Request $request): CrudModel
     {
+        $userId = auth()->id();
+        $userToFollowId = $request->input('user_id');
+
+        $follow = Follow::query()
+            ->where([
+                'follower_id' => $userId,
+                'following_id' => $userToFollowId,
+            ])
+            ->exists();
+
+        if ($follow) {
+            throw ValidationException::withMessages([
+                'following_id' => 'Already followed',
+            ]);
+        }
+
         /** @var User $currentUser */
         $currentUser = User::query()
-            ->where('id', auth()->id())
+            ->where('id', $userId)
             ->with(['following'])
             ->firstOrFail();
 
         $userToFollow = User::query()
-            ->where('id', $request->input('user_id'))
+            ->where('id', $userToFollowId)
             ->firstOrFail();
 
         $currentUser->following()->attach($userToFollow->id);
--- tests/Feature/Controllers/FollowControllerTest.php
+++ tests/Feature/Controllers/FollowControllerTest.php
@@ -3,11 +3,15 @@
 namespace Tests\Feature\Controllers;
 
 use App\Models\Follow;
+use Database\Factories\FollowFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class FollowControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_new_entry_successfully_created_in_follows_table(): void
     {
         $userA = $this->createUser();
@@ -40,5 +44,36 @@ class FollowControllerTest extends TestCase
             'follower_id' => $userA->id,
             'following_id' => $userB->id,
         ]);
+    }
+
+    public function test_follow_fails_if_relationship_already_exists_duplicate(): void
+    {
+        $userA = $this->createUser();
+        $userB = $this->createUser();
+
+        FollowFactory::new()
+            ->relationship($userA->id, $userB->id)
+            ->create();
+
+        $this->assertDatabaseHas((new Follow)->getTable(), [
+            'follower_id' => $userA->id,
+            'following_id' => $userB->id,
+        ]);
+
+        $token = $this->createToken($userA);
+
+        $response = $this
+            ->withHeaders([
+                'Authorization' => 'Bearer '.$token,
+            ])
+            ->postJson('api/follow/follow-user', [
+                'user_id' => $userB->id,
+            ]);
+
+        $response
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonValidationErrors([
+                'following_id',
+            ]);
     }
 }
